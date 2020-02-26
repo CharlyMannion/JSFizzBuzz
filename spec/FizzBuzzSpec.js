be665a1c3b62f071cc79396e92394d66948fd091
Charlotte Mannion <charlottemannion100@gmail.com>
2020-02-26 09:37:03 +0000

    meets the conditions of the numbers equal 3, 5, or 15 or num

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -5,8 +5,20 @@ describe("FizzBuzz", function() {
     fizzBuzz = new FizzBuzz();
   });
 
-  it('should return fizz given a multiple of 3', function(){
+  it('should return fizz given a multiple of 3 to evaluate', function(){
     expect(fizzBuzz.evaluate(3)).toEqual("fizz");
+  });
+
+  it('should return buzz when given a multiple of 5 to evaluate', function(){
+    expect(fizzBuzz.evaluate(5)).toEqual("buzz");
+  });
+
+  it('should return fizzbuzz when given a multiple of both 3 and 5', function(){
+    expect(fizzBuzz.evaluate(15)).toEqual("fizzBuzz");
+  });
+
+  it('should return the number is none of the conditions apply', function(){
+    expect(fizzBuzz.evaluate(7)).toEqual(7);
   });
 
 });
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,7 +1,14 @@
 function FizzBuzz() {
 }
 FizzBuzz.prototype.evaluate = function(num) {
+  if (num === 15) {
+    return "fizzBuzz";
+  };
   if (num === 3) {
     return "fizz";
   };
+  if (num === 5) {
+    return "buzz";
+  };
+  return num;
 };
