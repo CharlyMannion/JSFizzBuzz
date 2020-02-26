6f7ff9b067299e48aa243121235a2e9a9882419e
Charlotte Mannion <charlottemannion100@gmail.com>
2020-02-26 09:38:15 +0000

    refactors to use multiples of the fizzbuzz numbers

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,13 +1,13 @@
 function FizzBuzz() {
 }
 FizzBuzz.prototype.evaluate = function(num) {
-  if (num === 15) {
+  if (num % 15 === 0) {
     return "fizzBuzz";
   };
-  if (num === 3) {
+  if (num % 3 === 0) {
     return "fizz";
   };
-  if (num === 5) {
+  if (num % 5 === 0) {
     return "buzz";
   };
   return num;
